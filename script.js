f77da69ff62a63b494be96ad7221f8acbd0e9027
Victor Volkov <vityan2504@gmail.com>
2023-12-13 18:51:28 +0300

    Написал функцию для получения и вывода данных

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+const select = document.getElementById('cars');
+const text = document.getElementById('text');
+
+const getData = () => {
+   return fetch('./cars.json').then(res => res.json()).catch(err => console.log(err.message));
+};
+
+const renderCars = () => {
+   getData().then(data => {
+      data.cars.forEach(item => {
+         const option = document.createElement('option');
+         option.value = item.model;
+         option.textContent = item.brand;
+         select.append(option);
+      })
+   }).catch(err => console.log(err.message));
+}
+
+const showCarsInfo = (option) => {
+   if (option.value === '') text.textContent = option.textContent;
+
+   getData().then(data => {
+      data.cars.forEach(item => {
+         if (option.value === item.model && option.textContent === item.brand)
+            text.innerHTML =  `<div>Тачка: ${item.brand} ${item.model}</div><div>Цена: ${item.price}$</div>`;
+      });
+   }).catch(err => console.log(err.message));
+}
+
+
+select.addEventListener('change', () => {
+   const selectedOption = select.options[select.selectedIndex];
+   showCarsInfo(selectedOption);
+});
+
+renderCars();
